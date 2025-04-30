6ac52f355351e0c17db74304eb5a64a395618ea5
babakabulldozer <94636162+shivaram235vemula@users.noreply.github.com>
2025-04-30 13:31:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,9 @@
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "thisiscreatedon30april2025heyyoumanwhatsusfdjlkjejp"  # Must be globally unique!
+  force_destroy = true                       # Optional: allows bucket deletion even if non-empty
 
   tags = {
-    Name = "main"
-  }
-}
-
-
-resource "aws_subnet" "public-subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "public-subnet"
-  }
-}
-resource "aws_subnet" "private-subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "igw"
+    Environment = "Dev"
+    Owner       = "YourName"
   }
 }
