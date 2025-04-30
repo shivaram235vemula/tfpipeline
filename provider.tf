6e367511cac8204916bcda20deff95b222fb7375
babakabulldozer <94636162+shivaram235vemula@users.noreply.github.com>
2025-04-30 13:30:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,11 +2,20 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.51.1"
+      version = "5.64.0"
     }
   }
 }
 
 provider "aws" {
-  # Configuration options
+  region = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "timingremotestatebucket"
+    key    = "LockID"
+    region = "ap-south-1"
+    use_lockfile = true
+  }
 }
